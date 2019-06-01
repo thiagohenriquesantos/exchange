1f985328970bca7d102ba1e23dc99e4b92ac023d
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2019-06-01 17:35:40 -0300

    add peso argentina (ARS) and peso uruguay (UYU)

--- app/helpers/exchanges_helper.rb
+++ app/helpers/exchanges_helper.rb
@@ -2,10 +2,10 @@ module ExchangesHelper
 
   def currency_list
     [
-      'AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP',
+      'ARS', 'AUD', 'BGN', 'BRL', 'CAD', 'CHF', 'CNY', 'CZK', 'DKK', 'EUR', 'GBP',
       'HKD', 'HRK', 'HUF', 'IDR', 'ILS', 'INR', 'JPY', 'KRW', 'MXN',
       'MYR', 'NOK', 'NZD', 'PHP', 'PLN', 'RON', 'RUB', 'SEK', 'SGD',
-      'THB', 'TRY', 'USD', 'ZAR'
+      'THB', 'TRY', 'USD', 'UYU', 'ZAR'
     ]
   end
 end
